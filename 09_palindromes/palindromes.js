ae0dffc7c70bacbc0dfdbb0ba465b9f42a81cd2d
Contrigra <gaponov60b@gmail.com>
2023-03-08 11:47:16 +0300

    palindrome: finished

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
+const palindromes = function (s) {
+    let cleanString = s.replaceAll(/[^A-Z]/gi, '').toLowerCase()
+    let stringArray = cleanString.split('')
+    let reversedArray = [...stringArray].reverse()
 
+
+    return JSON.stringify(stringArray) === JSON.stringify(reversedArray);
 };
 
+console.log(palindromes('racecar!'));
 // Do not edit below this line
 module.exports = palindromes;
